a16bcf01f46deda282ce3d817def070bfd4b9b42
yuhang <yuhang.li@alibaba-inc.com>
2015-11-12 14:44:55 +0800

    // 提交代码

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by yuhang on 15/11/12.
+//  Copyright © 2015年 yuhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by yuhang on 15/11/12.
+//  Copyright © 2015年 yuhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
